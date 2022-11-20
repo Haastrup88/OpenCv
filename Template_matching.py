786036253ffa4141826aee989119cd8609ddb098
Haastrup88 <haastrupbay012@gmail.com>
2022-11-20 13:50:14 +0100

    commit

---
+++ Template_matching.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+img=cv2.imread("lena.jpg")
+ImgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+template=cv2.imread("lena_img.PNG",0)
+w,h=template.shape[::-1]
+res=cv2.matchTemplate(ImgGray,template,cv2.TM_CCOEFF_NORMED)
+threshold=0.98
+loc=np.where(res>=threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img,pt,(pt[0]+w,pt[1]+h),(0,0,255),2)
+print(loc)
+
+cv2.imshow("Img",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
